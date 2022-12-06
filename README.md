0b0059b11beccbb84a5f0446676bf045ec7fb829
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-12-06 15:01:25 -0600

    Demonstration Complete (#1)
    Sends emails, makes sure that you won't run into time limits, and marks entries that have been sent.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # mailMerge-demo
+
+This is a demonstration for how to use [sheetCore](https://github.com/texas-mcallen-mission/sheetCore) to make a mail-merge type application.
+
+The create-the-email function should be fairly simple to use and modify.
+
+## How to use this in your own project:
+
+1. Fork this repo!
+2. Create / open up a google Spreadsheet, and set up an AppsScript project for it. (Extensions / AppsScript)
+3. Set up your CI secrets so that things automatically push [following this guide](https://github.com/texas-mcallen-mission/deploy-google-app-script-action-typescript/README.md)
+
+Configure your column definitions to match ``emailSheetConfig.initialColumnOrder``
+* note: column names do not have to be the same as the key, but make it reasonable.
+* Also, if ``includeSoftcodedColumns`` is set to true, any columns with a header that aren't coded in will be included as long as they have unique names.
+
+Once everything's setup properly, your CI run should look something like this:
+![Successful CI Run](docs/successful%20CI%20run.png) 
+
+The first time you run ``mailhandlerdemo()``, a new sheet will appear in your spreadsheet with the columns as you've defined them.  Feel free to populate that and test with whatever you'd like!
---
+++ mailHandler.ts
@@ -0,0 +1,135 @@
+let emailSheetConfig:sheetDataEntry = {
+    tabName: 'data',
+    headerRow: 0,
+    initialColumnOrder: {
+        name: 0,
+        email: 1,
+        emailSent: 2,
+        stat1: 3,
+        stat2: 4,
+        stat3: 5,
+    },
+    includeSoftcodedColumns: true
+}
+
+function emailSender_(dataEntry: {}) {
+    let mailData: GoogleAppsScript.Mail.MailAdvancedParameters = {
+        name: "Bert's Messaging Bot",
+        subject:"Demo Email Thingy"
+    };
+    // list of available properties:
+    //     attachments ?: Base.BlobSource[] | undefined;
+    //     /** a comma-separated list of email addresses to BCC */
+    //     bcc ?: string | undefined;
+    //     /** the body of the email */
+    //     body ?: string | undefined;
+    //     /** a comma-separated list of email addresses to CC */
+    //     cc ?: string | undefined;
+    //     /** if set, devices capable of rendering HTML will use it instead of the required body argument; you can add an optional inlineImages field in HTML body if you have inlined images for your email */
+    //     htmlBody ?: string | undefined;
+    //     /** a JavaScript object containing a mapping from image key (String) to image data (BlobSource); this assumes that the htmlBody parameter is used and contains references to these images in the format <img src="cid:imageKey" /> */
+    //     inlineImages ?: { [imageKey: string]: Base.BlobSource } | undefined;
+    //     /** the name of the sender of the email (default: the user's name) */
+    //     name ?: string | undefined;
+    //     /** true if the email should be sent from a generic no-reply email address to discourage recipients from responding to emails; this option is only possible for G Suite accounts, not Gmail users */
+    //     noReply ?: boolean | undefined;
+    //     /** an email address to use as the default reply-to address (default: the user's email address) */
+    //     replyTo ?: string | undefined;
+    //     /** the subject of the email */
+    //     subject ?: string | undefined;
+    //     /** the address of the recipient */
+    //     to ?: string | undefined;
+    // 
+
+    if (dataEntry.hasOwnProperty("email")) {
+        mailData.to = dataEntry["email"];
+    }
+    let newline = "\n"
+    let body = "Hello, " + dataEntry["name"] + "!" + newline + newline
+        + "here's some demo numbers in case you want them:" + newline
+    
+    // the key here is the value of the desired column in the supplied sheetDataConfig
+    // the value is the displayed value in the thing.
+    // 
+    let stats = {
+        stat1: "Stat 1",
+        stat2: "Stat 2",
+        stat3: "Stat 3"
+    }
+    
+    for (let key in stats) {
+        if (dataEntry.hasOwnProperty(key)) {
+            body += stats[key] + ": " + dataEntry[key] + newline
+        }
+    }
+    mailData.body = body
+    MailApp.sendEmail(mailData)
+
+}
+
+function mailhandlerDemo() {
+    // record startup time so we don't go over:
+    let startTime = new Date()
+    let maxTimeInMinutes = 5 // for non-enterprise accounts, the limit for a single execution is like 6 minutes, for enterprise it can be up to ~30 minutes
+    // set up sheetData, get thing configured
+    let emailConfig = new RawSheetData(emailSheetConfig)
+    let emailSheet = new SheetData(emailConfig)
+    // load up data and stick it in a kiDataClass
+    let emailData = new kiDataClass(emailSheet.getData())
+    // add iterant so that we can do partial updates.  Might make this internal to the sheetData class in the future.
+    let iterantKey = "iterantKey"
+    emailData.addIterant(iterantKey, 0)
+    // filter out entries that have already been emailed
+    emailData.removeMatchingByKey("emailSent", [true])
+    
+    
+    if (emailData.data.length == 0) {
+        console.log("no new entries to email")
+        return
+    }
+    let emailedEntries:number[] = []
+    let emailOutData:any[] = []
+    // okay, onto the big fun loop.
+    for (let entry of emailData.end) {
+        if (canEmail_() && checkTime_(startTime, maxTimeInMinutes)) { // this saves us from trying to email when we can't and keeps out errors
+            emailSender_(entry)
+            emailedEntries.push(entry[iterantKey])
+            emailOutData.push({"emailSent":true})
+        } else {
+            break
+        }
+    }
+
+    for (let i = 0; i < emailedEntries.length; i++){
+        let entryPosition = emailedEntries[i] + 1
+        let modData = emailOutData[i]
+        emailSheet.directModify(entryPosition, modData)
+    }
+    
+
+}
+
+/**
+ *  Checks to make sure that the system isn't going to fail to finish because it went overtime. 
+ *  borrowed from github.com/texas-mcallen-mission/mileageLogHandler
+ * @param {Date} startTime
+ * @return {*}  {boolean}
+ */
+function checkTime_(startTime: Date, maxTimeInMinutes: number): boolean {
+    let currentTime = new Date();
+    let minuteToMillis = maxTimeInMinutes * 60000;
+    if (currentTime.getTime() - startTime.getTime() < minuteToMillis) {
+        return true;
+    } else {
+        console.log("Running out of time!");
+        return false;
+    }
+}
+
+function canEmail_(): boolean{
+    if (MailApp.getRemainingDailyQuota() > 15) {
+        return true
+    } else {
+        return false
+    }
+}
